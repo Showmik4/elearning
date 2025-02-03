2dbbd2a2be1159d8f83eed4f9287101ec75e570b
Showmik4 <showmik.aiub@gmail.com>
2025-02-04 03:09:46 +0600

    session maintain

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,50 +6,37 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\URL;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
     protected $redirectTo = RouteServiceProvider::HOME;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
     }
 
-    public function login(Request $request)
+    public function showLoginForm()
+    {        
+        Session::put('previous_url', URL::previous());
+        return view('auth.login');
+    }
+
+    public function login(Request $request): RedirectResponse
     {
-        $this->validate($request, [
+        $validated = $this->validate($request, [
             'email' => 'required|exists:user,email|email|max:50',
             'password' => 'required|string|min:8',
         ]);
-    
+
         $user = User::where('email', $request->email)->first();
     
         if ($user) {
@@ -64,11 +51,9 @@ class LoginController extends Controller
     
             Session::flash('error', 'Invalid Authentication');
             return redirect()->back()->withInput();
-        }
-    
+        } 
         Session::flash('error', 'You are not registered');
-        return redirect()->back()->withInput();
+        return redirect()->back()->withInput();  
+
     }
-    
-    
 }

b8aa4a7836fdffc98f27826dcc5a98800b311fca
Showmik4 <showmik.aiub@gmail.com>
2025-02-03 00:21:10 -0800

    updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,57 +1,6 @@
 @extends('layouts.main')
 @section('header.css')
-    <style>
-        /* .order-card {
-            color: #fff;
-        }
-
-        .bg-c-blue {
-            background: linear-gradient(45deg, #4099ff, #73b4ff);
-        }
-
-        .bg-c-green {
-            background: linear-gradient(45deg, #2ed8b6, #59e0c5);
-        }
-
-        .bg-c-yellow {
-            background: linear-gradient(45deg, #FFB64D, #ffcb80);
-        }
-
-        .bg-c-pink {
-            background: linear-gradient(45deg, #FF5370, #ff869a);
-        }
-
-
-        .card {
-            border-radius: 5px;
-            -webkit-box-shadow: 0 1px 2.94px 0.06px rgba(4, 26, 55, 0.16);
-            box-shadow: 0 1px 2.94px 0.06px rgba(4, 26, 55, 0.16);
-            border: none;
-            margin-bottom: 30px;
-            -webkit-transition: all 0.3s ease-in-out;
-            transition: all 0.3s ease-in-out;
-        }
-
-        .card .card-block {
-            padding: 25px;
-        }
-
-        .order-card i {
-            font-size: 26px;
-        }
-
-        .f-left {
-            float: left;
-        }
-
-        .f-right {
-            float: right;
-        } */
-        /* #barchart_material, #barchart_material1, #barchart_material2, #barchart_material3 {
-            max-width: 100%;
-            height: 300px;
-        } */
-    </style>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 @endsection
 @section('main.content')
     <div class="page-body">
@@ -71,11 +20,76 @@
             </div>
         </div>
         <!-- Container-fluid starts-->
-        <div class="container-fluid">
-            <div class="row second-chart-list third-news-update">
-               
+    <!-- Include Font Awesome -->
+
+
+    <div class="container-fluid py-4">
+        <div class="row">
+            <!-- Total Users Card -->
+            <div class="col-md-3">
+                <div class="card bg-primary text-white shadow-lg rounded-2xl p-4">
+                    <div class="card-body d-flex align-items-center">
+                        <div class="me-3">
+                            <i class="fa-solid fa-users fa-2x"></i>
+                        </div>
+                        <div>
+                            <h5 class="fw-bold">Total Users</h5>
+                            <h3>1,234</h3>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>       
+
+            <!-- Sales Card -->
+            <div class="col-md-3">
+                <div class="card bg-success text-white shadow-lg rounded-2xl p-4">
+                    <div class="card-body d-flex align-items-center">
+                        <div class="me-3">
+                            <i class="fa-solid fa-cart-shopping fa-2x"></i>
+                        </div>
+                        <div>
+                            <h5 class="fw-bold">Total Sales</h5>
+                            <h3>$12,345</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Orders Card -->
+            <div class="col-md-3">
+                <div class="card bg-warning text-white shadow-lg rounded-2xl p-4">
+                    <div class="card-body d-flex align-items-center">
+                        <div class="me-3">
+                            <i class="fa-solid fa-box fa-2x"></i>
+                        </div>
+                        <div>
+                            <h5 class="fw-bold">Total Orders</h5>
+                            <h3>567</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Revenue Card -->
+            <div class="col-md-3">
+                <div class="card bg-danger text-white shadow-lg rounded-2xl p-4">
+                    <div class="card-body d-flex align-items-center">
+                        <div class="me-3">
+                            <i class="fa-solid fa-dollar-sign fa-2x"></i>
+                        </div>
+                        <div>
+                            <h5 class="fw-bold">Total Revenue</h5>
+                            <h3>$89,567</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
         
     </div>  
+@endsection
+@section('footer.js')
+
 @endsection
